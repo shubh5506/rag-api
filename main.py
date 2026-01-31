cd63a38c5f66e668e67d075d1bd50d0887d98443
shubh5506 <sarpal75@gmail.com>
2026-01-30 23:47:02 -0600

    ingest files add change

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def health():
     return {"status": "ok"}
 
 
-@app.post("/ingest", include_in_schema=False)
+@app.post("/ingest")
 async def ingest(files: List[UploadFile] = File(...)):
     indexed_files = 0
     total_chunks = 0
